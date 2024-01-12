2e0d1495ad075aa5dbe71a112d52b5918436c755
Shruti kumari <87003332+123soniyo@users.noreply.github.com>
2024-01-12 08:40:28 +0000

    getElementById Method

---
+++ getElementById Method/demo.js
@@ -0,0 +1,18 @@
+let mainHedaing=document.getElementById('main-heading');
+mainHedaing.textContent='Fruti World'
+
+let head=document.getElementById('head')
+head.style.backgroundColor ='orange';
+head.style.color='white'
+head.style.borderBottom='4px solid green'
+
+
+let basketHeading=document.getElementById('basket-heading')
+basketHeading.style.color='green'
+
+
+let thanks=document.getElementById('thanks')
+thanks.innerHTML='<p> thank you for visting us</p>'
+thanks.style.color='green'
+thanks.style.fontWeight='bold'
+thanks.style.margin='10px'
